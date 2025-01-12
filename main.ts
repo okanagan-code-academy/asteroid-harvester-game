f91d679900c293eb1765ebf08750776db0af52e7
okanagan-code-academy <157773119+okanagan-code-academy@users.noreply.github.com>
2025-01-12 01:28:41 -0800

    Update pxt.json, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,60 @@
-let mySprite: Sprite = sprites.create(sprites.duck.duck1);
+namespace SpriteSheet{
+    export let playerImage: Image = assets.image`playerShip`
 
-let angle: number = 0;
+}
+//% promise
 
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    // At the moment have to reset the image because the algorithm is lossy
-    mySprite.setImage(sprites.duck.duck1);
-    angle += Math.PI / 12;
-    rotsprite.rotSprite(mySprite, angle)
-})
+let playerSprite: Sprite = sprites.create(SpriteSheet.playerImage);
+sprites.setDataNumber(playerSprite, "currentAngle", 0)
+sprites.setDataNumber(playerSprite, "desiredAngle", 0)
+let previousAction: string = "none"
 
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    // At the moment have to reset the image because the algorithm is lossy
-    mySprite.setImage(sprites.duck.duck1);
-    angle -= Math.PI / 12;
-    rotsprite.rotSprite(mySprite, angle)
-})
 
-forever(function(){
-    if(controller.left.isPressed()) {
-        mySprite.setImage(sprites.duck.duck1);
-        angle += Math.PI / 12;
-        rotsprite.rotSprite(mySprite, angle)
-    }
-})
+
+async function turnLeft() {
+    
+    timer.throttle(previousAction, 500, function () {
+        smoothRotate(playerSprite, - Math.PI / 2, 100)
+    })
+}
+async function turnRight() {
+    previousAction = "move"
+    timer.throttle(previousAction, 500, () => smoothRotate(playerSprite, Math.PI / 2, 10))
+}
+
+
+
+turnRight()
+turnRight()
+
+function smoothRotate(sprite: Sprite, rotationAngle: number, stepsize: number): void {
+    previousAction = "move"
+    let currentAngle: number = sprites.readDataNumber(sprite, "currentAngle")
+    let desiredAngle: number = currentAngle + rotationAngle
+    sprites.setDataNumber(playerSprite, "desiredAngle", desiredAngle)
+    let rotationRate: number = 0.1 * Math.sin(rotationAngle) / stepsize
+
+    // for(let step = 0; step < stepsize; step++){
+    //     let interpolatedValue: number = lerp(sprites.readDataNumber(playerSprite, "currentAngle"), sprites.readDataNumber(playerSprite, "desiredAngle"), step*(1/stepsize))
+    //     sprites.setDataNumber(playerSprite, "currentAngle", interpolatedValue)
+    //     playerSprite.setImage(SpriteSheet.playerImage)
+    //     rotsprite.rotSprite(playerSprite, sprites.readDataNumber(playerSprite, "currentAngle"))
+    //     pause(100)
+    // }
+
+    forever(function (): void {
+        let currentAngle: number = sprites.readDataNumber(playerSprite, "currentAngle")
+        let desiredAngle: number = sprites.readDataNumber(playerSprite, "desiredAngle")
+        let interpolatedValue: number = lerp(sprites.readDataNumber(playerSprite, "currentAngle"), sprites.readDataNumber(playerSprite, "desiredAngle"), 0.125)
+        sprites.setDataNumber(playerSprite, "currentAngle", interpolatedValue)
+        playerSprite.setImage(SpriteSheet.playerImage)
+        rotsprite.rotSprite(playerSprite, sprites.readDataNumber(playerSprite, "currentAngle"))
+    })
+}
+
+
+
+
+function lerp(value0: number, value1: number, t: number): number {
+    return value0 + t * (value1 - value0);
+}
